125f60d7c1423574326210466e6d9fa8d4ba2793
archana mandhare <amandhare@pmc.com>
2015-07-29 12:29:07 +0530

    Update pmc-theme-unit-test.php

--- pmc-theme-unit-test.php
+++ pmc-theme-unit-test.php
@@ -16,8 +16,9 @@ define ( 'PMC_THEME_UNIT_TEST_ERROR_LOG_FILE', PMC_THEME_UNIT_TEST_ROOT . '/erro
 
 function pmc_theme_unit_test_loader() {
 	
-	require_once PMC_THEME_UNIT_TEST_ROOT. '/pmc-singleton.php';
+	
 	require_once PMC_THEME_UNIT_TEST_ROOT. '/config.php';
+	require_once PMC_THEME_UNIT_TEST_ROOT. '/classes/pmc-singleton.php';
 	require_once PMC_THEME_UNIT_TEST_ROOT. '/classes/autoloader.php';
 
 	\PMC\Theme_Unit_Test\Admin::get_instance();
