d14c965ff3c87589500834a08e93a7e2c6695ed6
archana mandhare <amandhare@pmc.com>
2015-07-30 15:14:17 +0530

    Update pmc-theme-unit-test.php

--- pmc-theme-unit-test.php
+++ pmc-theme-unit-test.php
@@ -12,6 +12,8 @@ define ( 'PMC_THEME_UNIT_TEST_ROOT', __DIR__ );
 define ( 'PMC_THEME_UNIT_TEST_VERSION', '1.0' );
 define ( 'PMC_THEME_UNIT_TEST_IMPORT_LOG_FILE', PMC_THEME_UNIT_TEST_ROOT . '/import.log' );
 define ( 'PMC_THEME_UNIT_TEST_ERROR_LOG_FILE', PMC_THEME_UNIT_TEST_ROOT . '/error.log' );
+define ( 'PMC_THEME_UNIT_TEST_DUPLICATE_LOG_FILE', PMC_THEME_UNIT_TEST_ROOT . '/duplicate.log' );
+
 
 
 function pmc_theme_unit_test_loader() {
